6cb0345d901154e3c6aea1b07d5aa20521c3ee94
WongYueKeung <mikehuang0755@gmail.com>
2019-11-23 21:41:06 +1030

    split airbase related function from index controll to airbase controller

--- application/index/controller/Airbase.php
+++ application/index/controller/Airbase.php
@@ -17,9 +17,271 @@ use think\helper\Time;
 
 class Airbase extends Controller
 {
-    public function index()
-    {
-        echo 'hi';
+    public function read_state_blue_airbase($data){
+        //$data  = $this -> read_state();
+        //dump($data['baseOwnership']['airbases']['blue']);
+        $data = $data['baseOwnership']['airbases']['blue'];
+        return $data;
+    }
+    public function read_state_red_airbase($data){
+        //$data  = $this -> read_state();
+        //dump($data['baseOwnership']['airbases']['red']);
+        $data = $data['baseOwnership']['airbases']['red'];
+        return $data;
+
+    }
+
+
+    /**
+     * @param $blue_current_airbase an array contain Blue current airbases, has to be from RSRstate.json file
+     * @return String json string for the mapbox,  inside the js
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function blue_json_generator_airbase($blue_current_airbase){
+        //$blue_current_airbase = $this->read_state_blue_airbase();
+
+
+        foreach ($blue_current_airbase as $key => $value){
+
+            $db_temp = CaucasusLivemap::where('name', $value)->find();
+            //dump($value);
+            //dump($db_temp);
+            $data[$key]['type'] = 'Feature';
+            $data_properties['cap'] = $db_temp['display_name'];
+            $data_properties['uncap'] = 'small text';
+
+            $data[$key]['properties'] = $data_properties;
+
+            $data_geometry['type'] = "Point";
+            $data_coordinates = array($db_temp['x'],$db_temp['y']);
+            $data_geometry['coordinates'] = $data_coordinates;
+
+            $data[$key]['geometry'] = $data_geometry;
+
+
+            unset($data_properties, $data_geometry,$data_coordinates);
+        }
+        //dump(json_encode($data));
+
+
+        //return $data;
+
+        //start to merge this array in to json for mapbox web GL
+        $json =  '{
+            "id":"blue",
+            "type":"symbol",
+            "source":{
+                "type":"geojson",
+                "data":{
+                    "type":"FeatureCollection",
+                    "features":[
+                        {
+                            "type":"Feature",
+                            "properties":{
+                                "cap":"Sochi",
+                                "uncap":"small text"
+                            },
+                            "geometry":{
+                                "type":"Point",
+                                "coordinates":[
+                                    39.729996,
+                                    43.58835
+                                ]
+                            }
+                        },
+                        {
+                            "type":"Feature",
+                            "properties":{
+                                "cap":"Krasnodar",
+                                "uncap":"small text"
+                            },
+                            "geometry":{
+                                "type":"Point",
+                                "coordinates":[
+                                    38.97626,
+                                    45.057762
+                                ]
+                            }
+                        }
+                    ]
+                }
+            },
+            "layout":{
+                "icon-image": "pulsing-dot1",
+                "text-field":[
+                    "format",
+                    [
+                        "upcase",
+                        [
+                            "get",
+                            "cap"
+                        ]
+                    ],
+                    {
+                        "font-scale":0.8
+                    },
+                    "\n",
+                    {
+
+                    },
+                    [
+                        "downcase",
+                        [
+                            "get",
+                            "uncap"
+                        ]
+                    ],
+                    {
+                        "font-scale":0.6
+                    }
+                ],
+                "text-font":[
+                    "Open Sans Semibold",
+                    "Arial Unicode MS Bold"
+                ],
+                "text-offset":[
+                    0,
+                    0.6
+                ],
+                "text-anchor":"top"
+            }
+        }';
+
+        //make json data for key:features
+        $json_decode = json_decode($json, 1);
+        $json_decode['source']['data']['features'] = $data;
+
+
+        //temp fix json decode issue
+        $result = str_replace('[]' , '{}',json_encode($json_decode));
+        return $result;
+
+    }
+
+    /**
+     * @param $red_current_airbase an array contain Blue current airbases, has to be from RSRstate.json file
+     * @return String json string for the mapbox,  inside the js
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function red_json_generator_airbase($red_current_airbase){
+        //$blue_current_airbase = $this->read_state_red_airbase();
+
+        foreach ($red_current_airbase as $key => $value){
+
+            $db_temp = CaucasusLivemap::where('name', $value)->find();
+            //dump($value);
+            //dump($db_temp);
+            $data[$key]['type'] = 'Feature';
+            $data_properties['cap'] = $db_temp['display_name'];
+            $data_properties['uncap'] = 'small text';
+
+            $data[$key]['properties'] = $data_properties;
+
+            $data_geometry['type'] = "Point";
+            $data_coordinates = array($db_temp['x'],$db_temp['y']);
+            $data_geometry['coordinates'] = $data_coordinates;
+
+            $data[$key]['geometry'] = $data_geometry;
+
+
+            unset($data_properties, $data_geometry,$data_coordinates);
+        }
+        //dump(json_encode($data));
+
+
+        //return $data;
+
+        $json =  '{
+            "id":"red",
+            "type":"symbol",
+            "source":{
+                "type":"geojson",
+                "data":{
+                    "type":"FeatureCollection",
+                    "features":[
+                        {
+                            "type":"Feature",
+                            "properties":{
+                                "cap":"Sochi",
+                                "uncap":"small text"
+                            },
+                            "geometry":{
+                                "type":"Point",
+                                "coordinates":[
+                                    39.729996,
+                                    43.58835
+                                ]
+                            }
+                        },
+                        {
+                            "type":"Feature",
+                            "properties":{
+                                "cap":"Krasnodar",
+                                "uncap":"small text"
+                            },
+                            "geometry":{
+                                "type":"Point",
+                                "coordinates":[
+                                    38.97626,
+                                    45.057762
+                                ]
+                            }
+                        }
+                    ]
+                }
+            },
+            "layout":{
+                "icon-image":"pulsing-dot",
+                "text-field":[
+                    "format",
+                    [
+                        "upcase",
+                        [
+                            "get",
+                            "cap"
+                        ]
+                    ],
+                    {
+                        "font-scale":0.8
+                    },
+                    "\n",
+                    {
+
+                    },
+                    [
+                        "downcase",
+                        [
+                            "get",
+                            "uncap"
+                        ]
+                    ],
+                    {
+                        "font-scale":0.6
+                    }
+                ],
+                "text-font":[
+                    "Open Sans Semibold",
+                    "Arial Unicode MS Bold"
+                ],
+                "text-offset":[
+                    0,
+                    0.6
+                ],
+                "text-anchor":"top"
+            }
+        }';
+
+        //make json data for key:features
+        $json_decode = json_decode($json, 1);
+        $json_decode['source']['data']['features'] = $data;
+
+        $result = str_replace('[]' , '{}',json_encode($json_decode));
+
+        return $result;
 
     }
 
